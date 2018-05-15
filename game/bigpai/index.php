fe07ab18db2d935454d320da120eb6933a2f3264
geekerstar <247507792@qq.com>
2018-05-15 16:19:52 +0800

    byb

---
+++ game/bigpai/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '{"code":1000,"message":"获取排行榜成功","result":[{"user_id":4021,"score":"1230","headimgurl":"http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/Q0j4TwGTfTIb8iaic3oN5datsuu9JLAEjpqVySt3IjbzibrjNjjYq12OiacjQUWBVeYODTgWFoat6r9rUfB9uUWnXg\/132","nickname":"Geekerstar"},
+{"user_id":4010,"score":"1003","headimgurl":"http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/Bn9AD2icWvdNrXSyV5PZC19iaLXG6dUyTNpG08n2wXjK5DrLhwfK9vX9cFc9wTNzkoWVRVekQoDiazeUicbTeECfIA\/132","nickname":"落花随风"},
+{"user_id":3906,"score":"864","headimgurl":"http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/OgUyRLfMoTAseGH9FAibEn8ASyRNhtJHBEGEBfsdmoYTkjGWZibwDkClBibA1pp4l59LgDXyQS8p8R0Wbgmfh8hgQ\/132","nickname":"Braveheart"},
+{"user_id":4009,"score":"643","headimgurl":"http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/w4SGhcYMkAibyejcI9cwuDfCbGB8zKv0zhmnF4icvibicfhfGAuHE52kCJxLT3y2UfpLFN0fwQwrCgkYULKSotbZdA\/132","nickname":"动岚"},
+{"user_id":3911,"score":"234","headimgurl":"http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/AkI7IRfKPvp0XaPZ6IEP1BnoNh5mRajMxC8CO9AZ6PG5Ndt3ibKQiciaU09E1sHWhwvHYACtmWUaFz24rbnsNMpaw\/132","nickname":"哈哈"},
+{"user_id":3992,"score":"222","headimgurl":"http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/Xpia9WibuFNUzaRibbOa6jkReUnhlPssFNZEicicnoB56CkKiaWFGCXXqicfmG2hGHwdciaToN7eZf3licb5SJQ9UbMJ2VA\/132","nickname":"小黑妹"},
+]}';
+?>
